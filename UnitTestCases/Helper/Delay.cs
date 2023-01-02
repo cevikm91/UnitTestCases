58be158900cfdfa2a4418987d52ec2c66a5b6cb2
cevikm91 <m.cevik91@gmail.com>
2023-01-02 18:13:39 +0100

    add Delay class

---
+++ UnitTestCases/Helper/Delay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+
+namespace UnitTestCases.Helper
+{
+    public class Delay
+    {
+        public void InSeconds(int seconds) => Thread.Sleep(TimeSpan.FromSeconds(seconds));
+
+        public void InMilliSeconds(double miliSeconds) => Thread.Sleep(TimeSpan.FromMilliseconds(miliSeconds));
+
+    }
+}
