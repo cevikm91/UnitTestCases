f3ff0fd69c26bc9ded0ae0376565b19e4157e0b3
cevikm91 <m.cevik91@gmail.com>
2023-01-02 16:45:54 +0100

    first step to make 1 Test
    starting with 1 Test
    and editing .gitignore

--- UnitTestCases/UnitTest1.cs
+++ UnitTestCases/UnitTest1.cs
@@ -1,11 +1,40 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace UnitTestCases
+namespace UnitTestCases.Init
 {
     [TestClass]
     public class UnitTest1
     {
+
+        protected static Init NotepadPlusPlus;
+        private readonly Artikel Artikel = new Artikel(NotepadPlusPlus.Driver);
+        //private readonly Keyboard Keyboard = new Keyboard();
+        //private readonly Delay Delay = new Delay();
+
+        [ClassInitialize]
+        public static void CUPInitStart(TestContext _)
+        {
+            NotepadPlusPlus = new Init();
+            Delay Delay = new Delay();
+            //Thread ThreadDelay = new Thread(() => Delay.InSeconds(5));
+
+            NotepadPlusPlus.StartNotepadPlusPlus();
+            //NotepadPlusPlus.BringCUPtoFront();
+
+            //ThreadDelay.Start();
+            Delay.InSeconds(5);
+            //NotepadPlusPlus.BringCUPtoFront();
+            //CUP.Password.SendKeys(CUP.CUPPassword);
+
+            NotepadPlusPlus.Login.Click();
+            Delay.InSeconds(3);
+
+            //NotepadPlusPlus.BringCUPtoFront();
+        }
+
+
+
         [TestMethod]
         public void TestMethod1()
         {
